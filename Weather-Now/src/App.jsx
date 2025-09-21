c585dde801477ecc83b70279b36032ee26aae26a
alankritip <alankritipal2004@gmail.com>
2025-09-21 17:43:59 +0530

    Basic React and Tailwind setup

---
+++ Weather-Now/src/App.jsx
@@ -0,0 +1,13 @@
+import Recat from 'react'
+
+const App = () =>{
+  return (
+    <div>
+       <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+    </div>
+  )
+}
+
+export default App;
