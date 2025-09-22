a476464316772f9fc87b26dda7fda762b02afc2d
alankritip <alankritipal2004@gmail.com>
2025-09-22 13:31:34 +0530

    made changes according to the weather background page

--- Weather-Now/src/App.jsx
+++ Weather-Now/src/App.jsx
@@ -1,12 +1,28 @@
-import Recat from 'react'
+import React, { useState } from 'react'
+import WeatherBackground from './components/WeatherBackground';
 
 const App = () =>{
+  const [weather , setWeather] = useState(null);
+
+  const getWeatherCondition = ()=>{
+    weather && ({
+      main : weather.weather[0].main,
+      isDay : Date.now()/1000 > weather.sys.sunrise && Date.now() /1000 < weather.sys.sunset
+    })
+  }
   return (
-    <div>
-       <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <div className='min-h-screen'>
+      <WeatherBackground condition={getWeatherCondition()} />
+
+      <div className='flex items-center justify-center p-8 min-h-screen'>
+        <div className='bg-transparent backdrop-filter backdrop-blur-md rounded-xl shadow-2xl p-8 max-w-md text-yellow-500 w-full border border-blue/30 relative z-10'>
+        <h1 className='text-4xl font-extrabold text-center mb-6'> Weather Now</h1>
+
+        </div>
+
+      </div>
     </div>
+    
   )
 }
 
